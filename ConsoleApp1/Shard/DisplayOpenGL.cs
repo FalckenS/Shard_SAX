70a4a4e3cd0e236e03cd802f087fcded48a97322
Samuel Falck <samuelfalck@gmail.com>
2025-02-07 14:01:58 +0100

    Create DisplayOpenGL.cs

---
+++ ConsoleApp1/Shard/DisplayOpenGL.cs
@@ -0,0 +1,30 @@
+﻿namespace Shard
+{
+    class DisplayOpenGL : Display
+    {
+        public override void initialize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void clearDisplay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void display()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void showText(string text, double x, double y, int size, int r, int g, int b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void showText(char[,] text, double x, double y, int size, int r, int g, int b)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

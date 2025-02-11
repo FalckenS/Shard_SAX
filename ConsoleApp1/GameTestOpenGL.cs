7b6c6c739ddee83f8a0bcf58650535d44a3db4c2
Samuel Falck <samuelfalck@gmail.com>
2025-02-12 00:23:40 +0100

    Add OpenGL window and simple GameTest for it
    Add a OpenGL window and a Game GameTest. It starts by rendering a blue triangle, but after a couple of frames it renders a red one.

---
+++ ConsoleApp1/GameTestOpenGL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Shard
+{
+    class GameTestOpenGL : Game
+    {
+        private int _frame;
+
+        public override void initialize()
+        {
+            _frame = 0;
+        }
+
+        public override void update()
+        {
+            _frame++;
+            if (_frame == 200)
+            {
+                Console.WriteLine(_frame);
+                GameObject gameObject = new GameObject();
+                Bootstrap.getDisplay().addToDraw(gameObject);
+            }
+        }
+    }
+}

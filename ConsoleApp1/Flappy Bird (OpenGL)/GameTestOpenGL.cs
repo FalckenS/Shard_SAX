75dfb15bcbd2f35d1fd73692a03cb29955e2dd2c
FalckenS <samuelfalck@gmail.com>
2025-02-20 03:29:09 +0100

    Change OpenGL position-system to pixels

--- ConsoleApp1/Flappy Bird (OpenGL)/GameTestOpenGL.cs
+++ ConsoleApp1/Flappy Bird (OpenGL)/GameTestOpenGL.cs
@@ -1,35 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 
 namespace Shard;
 
-class GameTestOpenGL : Game
+internal class GameTestOpenGL : Game
 {
-    private int _frame;
-    private TextToRender hello;
-    private List<RectangleGameObject> _rectangleGameObjects;
-
     public override void initialize()
     {
-        _frame = 0;
-        hello = new TextToRender("Hello", 60.0f, 50.0f, 1, 255, 255, 255);
-        
         RectangleGameObject rectangle = new RectangleGameObject();
-        rectangle.Transform.X = -1;
+        rectangle.Transform.X = 0;
         rectangle.Transform.Y = 0;
-        rectangle.Transform.Ht = 1;
-        rectangle.Transform.Wid = 2;
+        rectangle.Transform.Wid = 100;
+        rectangle.Transform.Ht = 100;
     }
 
     public override void update()
     {
-        _frame++;
         Bootstrap.getDisplay().showText("Hello", 60.0f, 50.0f, 1, 255, 255, 255);
     }
 }
 
-class RectangleGameObject : GameObject, CollisionHandler
+internal class RectangleGameObject : GameObject, CollisionHandler
 {
     private int _minX;
     private int _minY;
@@ -39,17 +30,14 @@ class RectangleGameObject : GameObject, CollisionHandler
     
     public override void initialize()
     {
+        _display = Bootstrap.getDisplay();
+        
         setPhysicsEnabled();
         MyBody.Mass = 1;
         MyBody.Kinematic = true;
         MyBody.addRectCollider();
-        
-        _minX = (int)MyBody.MinAndMaxX[0];
-        _minY = (int)MyBody.MinAndMaxY[0];
-        _maxX = (int)MyBody.MinAndMaxX[1];
-        _maxY = (int)MyBody.MinAndMaxY[1];
-        
-        _display = Bootstrap.getDisplay();
+
+        UpdatePosition();
     }
     
     public override void update()

4bccad7b8f05b1528f0a44297b40badd9e9828c3
Alexander Lisborg <alexanderlisborg@gmail.com>
2025-03-02 20:31:34 +0100

    Add sprite, spriteBatch. Spritebatch intended to be used in batching when rendering sprites.

---
+++ ConsoleApp1/Shard/Sprite.cs
@@ -0,0 +1,35 @@
+﻿using Shard.Shard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    internal class Sprite
+    {
+        public float X { get; set; }
+        public float Y {  get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        private TextureRegion _textureRegion;
+        // Constructors
+        public Sprite(TextureRegion textureRegion, float x, float y, float width, float height) 
+        {
+            X = x;Y = y;Width = width;Height = height;_textureRegion = textureRegion;
+        }
+        public Sprite(TextureRegion textureRegion) : this(textureRegion,0,0,textureRegion.TextureWidth,textureRegion.TextureHeight) { }
+
+        public Sprite(TextureRegion textureRegion, float x, float y) : this(textureRegion,x,y,textureRegion.RegionWidth,textureRegion.TextureHeight){ }
+    
+        public Sprite(Texture texture, float rx, float ry, float rwidth, float rheight, float x, float y, float width, float height) : 
+            this(new TextureRegion(texture,rx,ry,rwidth,rheight),x,y,width,height){ }
+        public Sprite(Texture texture, float x, float y) : this(texture,0,0,texture.Width,texture.Height,x,y,texture.Width,texture.Height){ }
+        public Sprite(Texture texture, float x, float y, float width, float height) : this(texture,0,0,texture.Width,texture.Height,x,y,width,height){ }
+
+
+
+    }
+}
---
+++ ConsoleApp1/Shard/SpriteBatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    // Can be used for future optimiziations of sprite rendering by batching.
+    class SpriteBatch
+    {
+        private List<Sprite> _sprites;
+        
+        public void Draw(Sprite sprite) { _sprites.Add(sprite); }
+        public void Draw(Sprite[] sprites) { _sprites.Concat(sprites); }
+    }
+}

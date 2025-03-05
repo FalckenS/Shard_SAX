c2d1b5194671591b2fa14b5af8a4cac3b0d3974c
Alexander Lisborg <alexanderlisborg@gmail.com>
2025-03-05 02:26:27 +0100

    Added TextureLoaders and an enum in the bootstrap for identifying the displayEngine

---
+++ ConsoleApp1/Shard/TextureLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    interface TextureLoader
+    {
+        void Load(byte[] imgdata, int width, int height);
+        void Use();
+    }
+}
---
+++ ConsoleApp1/Shard/TextureLoaderFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard.Shard
+{
+    class TextureLoaderFactory
+    {
+        public static TextureLoader get() 
+        {
+            DisplayEngine current = Bootstrap.DisplayEngine;
+            switch (current)
+            {
+                case DisplayEngine.OpenGL: return new GLTextureLoader();
+                default:throw new NotSupportedException();
+
+            }
+        }
+    }
+}
